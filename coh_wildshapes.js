c8ba0e7ba4624a6610ffdf29e8b9df3f6e111a10
Reztroz <87739071+Reztroz@users.noreply.github.com>
2023-04-21 15:30:43 -0400

    Update coh_wildshapes.js

--- coh_wildshapes.js
+++ coh_wildshapes.js
@@ -24,5 +24,24 @@ CreatureList["dire goose"] = {
 		"Perception" : 4,
 		"Intimidation" : 2,
 	}
-		
+	actions : [{
+		name : "Multiattack",
+		description : "The goose makes two beak attacks.",
+	},
+	{
+		name: "Honk(1/Day).",
+		description : "The goose emits a terrifying honk in a 15-foot-radius sphere. Creatures that can hear the goose and are aware of it must make a DC 12 Wisdom saving throw or be frightened of the goose for 1 minute.",
+	}],
+	features : [{
+		name: " Fearless Fowl",
+		description : "The goose has the advantage on saving throws against being frightened",
+	},
+	{
+		name : "Hold Breath",
+		description : "The goose can hold its breath for 10 minutes while underwater",
+	},
+	{
+		name : "Savage Attacker(1/Short Rest)",
+		description : "When the goose rolls damage for a melee weapon attack, it can reroll the attack's damage dice and use either total.",
+	}],
 }
